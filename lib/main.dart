b338eb0e26be363ef1e8ab61ae69bcd3b7f7a827
riyadzaigirdar <riyad1401043@gmail.com>
2021-03-03 14:54:34 +0600

    added in readme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+
 import 'package:flutter/material.dart';
 
 
@@ -19,6 +20,10 @@ class MyApp extends StatelessWidget{
 }
 
 class MyHome extends StatelessWidget{
+
+  void printer(){
+    print("little bit");
+  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -28,85 +33,65 @@ class MyHome extends StatelessWidget{
           child:Text("My Awesome App")
           ),
       ),
-      body:ListView(
-        children: <Widget>[
-          Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            // margin: const EdgeInsets.all(10.0),
-            // padding: const EdgeInsets.all(20.0),
-            // decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.red),
-            decoration: BoxDecoration(
+      body: Center(
+              child: Container(
+                alignment: Alignment.centerLeft,
                 color: Colors.green,
-                border: Border.all(
-                  color: Colors.red[500],
+                width: 100,
+                height: 100,
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Colors.pink, Colors.yellow]
-                  ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    blurRadius: 10
-                  )
-                ]
               ),
-            margin: const EdgeInsets.all(10.0),
-            // color: Colors.red,
-            width: 150,
-            height: 100,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(margin: EdgeInsets.only(bottom: 10.0),child:Text("Total Candidate")),
-                Text("500")
-                ]
+      drawer: Drawer(
+        // elevation: 100,
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("riyad zaigirdar"), 
+              accountEmail: Text("riyadzaigir280@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/42799939?s=460&u=975ca8612ee36f030fc1952cd7ded42ce01efb69&v=4"),
+                ),
               ),
-        ),
-        Container(
-          margin: const EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-            color: Colors.red, 
-            borderRadius: BorderRadius.all(Radius.circular(10.0))),
-          // color: Colors.red,
-          width: 150,
-          height: 100,
-          child: Center(child: Text("hello hello hello hello", textAlign: TextAlign.center , style: TextStyle(fontSize: 20, color: Colors.white),)),
-        )
-        ],),
-        Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              color: Colors.blue, 
-              border: Border.all(color: Colors.red), 
-              borderRadius: BorderRadius.all(Radius.circular(10.0))),
-          // color: Colors.red,
-          width: 150,
-          height: 100,
-          child: Center(child: Text("hello hello")),
-        ),
-        Container(
-          margin: const EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-            color: Colors.yellow,
-            border: Border.all(color:Colors.black),
-            borderRadius: BorderRadius.all(Radius.circular(10.0))
+            // DrawerHeader(
+            //   child: Text("My Drawer"),
+            //   decoration: BoxDecoration(color: Colors.purple),
+            //   ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Riyad Zaigirdar"),
+              subtitle: Text("Software Developer"),
+              trailing: GestureDetector(child:Icon(Icons.edit), onDoubleTap: ()=>printer(),),
+              onTap: (){},
+              onLongPress: (){},
             ),
-          //color: Colors.red,
-          width: 150,
-          height: 100,
-          child: Center(child: Text("hello hello")),
-        )
-        ],)
-        ],
-      )     
+            ListTile(
+              leading:Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("riyadzaigir280@gmail.com"),
+              trailing: Icon(Icons.electric_bike),
+              onTap: (){},
+              onLongPress: (){},
+            )
+          ],
+        ),
+        ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (){},        
+        tooltip: "add a image",
+        icon: Icon(Icons.thumb_up),
+        label: Text("Like"),
+        backgroundColor: Colors.green,
+      ),
+    //   floatingActionButton: FloatingActionButton.extended(
+    //   onPressed: () {
+    //     // Add your onPressed code here!
+    //   },
+    //   label: Text('Approve'),
+    //   icon: Icon(Icons.thumb_up),
+    //   backgroundColor: Colors.pink,
+    // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
